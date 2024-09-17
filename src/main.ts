a64c8edd62011302ddd3a2213551348869247576
Jackie Tsien <jackie10673311@gmail.com>
2024-09-17 21:41:27 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "@kitware/vtk.js/Rendering/OpenGL/Profiles/Geometry";
+import "@kitware/vtk.js/Rendering/OpenGL/Profiles/Volume";
+import "@kitware/vtk.js/Rendering/OpenGL/Profiles/Glyph";
+
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
